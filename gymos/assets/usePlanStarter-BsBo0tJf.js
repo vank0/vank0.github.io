d13b5d634a034a1a402e1bd123a2bbb6487fab18
Ivan Ivanov <ivan.ivanov@nestle.com>
2026-08-02 20:34:53 +0300

    gladiator: fasting as a daily schedule, merged into Overview

---
+++ gymos/assets/usePlanStarter-BsBo0tJf.js
@@ -0,0 +1,1 @@
+import{u as e}from"./Gloss-DN7wpG49.js";import{l as t}from"./src-AdKYs3I5.js";import{U as n,ut as r}from"./index-DtIXaJnn.js";function i(){let i=e(),a=n();return{start:e=>a(async()=>{let n=await r(e);return n.ok?n.sessionId:(t({variant:`error`,message:i(`sched.routineGone`,`That routine was deleted.`)}),null)})}}export{i as t};
