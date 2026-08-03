ddebc5d1f6f94f3928f55212c701081fedf39b63
Ivan Ivanov <ivan.ivanov@nestle.com>
2026-08-03 15:02:17 +0300

    fix(interval): no cue before the first Play

---
+++ gymos/assets/usePlanStarter-BoeoRIrs.js
@@ -0,0 +1,1 @@
+import{u as e}from"./Gloss-3cXGrio4.js";import{l as t}from"./src-CjvXBZyL.js";import{U as n,ut as r}from"./index-aL684nTT.js";function i(){let i=e(),a=n();return{start:e=>a(async()=>{let n=await r(e);return n.ok?n.sessionId:(t({variant:`error`,message:i(`sched.routineGone`,`That routine was deleted.`)}),null)})}}export{i as t};
