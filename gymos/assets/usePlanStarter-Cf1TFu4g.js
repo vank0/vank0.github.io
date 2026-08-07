a7d34e8a42689db9c76916bb532e15aa2fb8d5a3
Ivan Ivanov <ivan.ivanov1@inceptionai.ai>
2026-08-07 10:59:14 +0300

    GymOS: concept info tooltips, custom WODs on the board, Apple-motion polish

---
+++ gymos/assets/usePlanStarter-Cf1TFu4g.js
@@ -0,0 +1,1 @@
+import{l as e}from"./src-Ct0xq9E9.js";import{l as t}from"./i18n-DbZCXKz6.js";import{k as n,nt as r}from"./index-BaADb7jQ.js";function i(){let i=t(),a=n();return{start:t=>a(async()=>{let n=await r(t);return n.ok?n.sessionId:(e({variant:`error`,message:i(`sched.routineGone`,`That routine was deleted.`)}),null)})}}export{i as t};
