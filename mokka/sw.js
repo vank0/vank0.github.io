ef0cacc157d6241de74c395754b4e000b88b1847
Ivan Ivanov <ivan.ivanov1@inceptionai.ai>
2026-08-18 15:54:54 +0300

    Update Blitz: horizon labels alternate above/below the axis

--- mokka/sw.js
+++ mokka/sw.js
@@ -2,8 +2,8 @@
    инжектират от Vite плъгина на build (public/ се копира дословно).
    Големите файлове (ръководството, 6.5MB) НЕ влизат в install списъка —
    те се кешират при първото отваряне, за да е бърза инсталацията. */
-const ASSETS = ["./assets/golos-text-cyrillic-400-normal-BwL4n7Pb.woff","./assets/golos-text-cyrillic-400-normal-C7us6pn1.woff2","./assets/golos-text-cyrillic-500-normal-BSLQUuP1.woff2","./assets/golos-text-cyrillic-500-normal-hXinzVVQ.woff","./assets/golos-text-cyrillic-700-normal-BKmY45Ip.woff2","./assets/golos-text-cyrillic-700-normal-ClsrbE7_.woff","./assets/golos-text-latin-400-normal-Coi1FYaD.woff2","./assets/golos-text-latin-400-normal-DOuJOmdK.woff","./assets/golos-text-latin-500-normal-BQo4s7Kn.woff","./assets/golos-text-latin-500-normal-BznAvurO.woff2","./assets/golos-text-latin-700-normal-CxmN_Nfd.woff2","./assets/golos-text-latin-700-normal-DAuVRgMH.woff","./assets/index-DuvNH2u4.css","./assets/index-wzMC4qpu.js","./icons/icon-192.png","./icons/icon-512.png","./index.html","./manifest.webmanifest","./manual-mokka-my16-bg.pdf","./protokol-ptp.pdf"];
-const CACHE = "blitz-7f71d3be3022";
+const ASSETS = ["./assets/golos-text-cyrillic-400-normal-BwL4n7Pb.woff","./assets/golos-text-cyrillic-400-normal-C7us6pn1.woff2","./assets/golos-text-cyrillic-500-normal-BSLQUuP1.woff2","./assets/golos-text-cyrillic-500-normal-hXinzVVQ.woff","./assets/golos-text-cyrillic-700-normal-BKmY45Ip.woff2","./assets/golos-text-cyrillic-700-normal-ClsrbE7_.woff","./assets/golos-text-latin-400-normal-Coi1FYaD.woff2","./assets/golos-text-latin-400-normal-DOuJOmdK.woff","./assets/golos-text-latin-500-normal-BQo4s7Kn.woff","./assets/golos-text-latin-500-normal-BznAvurO.woff2","./assets/golos-text-latin-700-normal-CxmN_Nfd.woff2","./assets/golos-text-latin-700-normal-DAuVRgMH.woff","./assets/index-2AQVxCgm.js","./assets/index-B9otQaSS.css","./icons/icon-192.png","./icons/icon-512.png","./index.html","./manifest.webmanifest","./manual-mokka-my16-bg.pdf","./protokol-ptp.pdf"];
+const CACHE = "blitz-0fad54ac3a6e";
 const LAZY = /manual-.*\.pdf$/;
 
 self.addEventListener('install', (e) => {
