e23a38da00571663ddee3d5a897fe5675b5a52e6
Ivan Ivanov <ivan.ivanov1@inceptionai.ai>
2026-08-19 19:16:36 +0300

    Сервиз: „Трансмисия“ става „Масло на скоростната кутия“ + климатик с количество фреон

--- mokka/sw.js
+++ mokka/sw.js
@@ -2,8 +2,8 @@
    инжектират от Vite плъгина на build (public/ се копира дословно).
    Големите файлове (ръководството, 6.5MB) НЕ влизат в install списъка —
    те се кешират при първото отваряне, за да е бърза инсталацията. */
-const ASSETS = ["./assets/golos-text-cyrillic-400-normal-C7us6pn1.woff2","./assets/golos-text-cyrillic-500-normal-BSLQUuP1.woff2","./assets/golos-text-cyrillic-700-normal-BKmY45Ip.woff2","./assets/golos-text-latin-400-normal-Coi1FYaD.woff2","./assets/golos-text-latin-500-normal-BznAvurO.woff2","./assets/golos-text-latin-700-normal-CxmN_Nfd.woff2","./assets/index-By2XxHsL.css","./assets/index-CWVyWRrJ.js","./assets/logbook-SZizV9sE.js","./icons/icon-192.png","./icons/icon-512.png","./icons/mark.svg","./index.html","./lights/CREDITS.md","./lights/tell-tales.ttf","./manifest.webmanifest","./manual-mokka-my16-bg.pdf","./protokol-ptp.pdf"];
-const CACHE = "blitz-8219071db04f";
+const ASSETS = ["./assets/golos-text-cyrillic-400-normal-C7us6pn1.woff2","./assets/golos-text-cyrillic-500-normal-BSLQUuP1.woff2","./assets/golos-text-cyrillic-700-normal-BKmY45Ip.woff2","./assets/golos-text-latin-400-normal-Coi1FYaD.woff2","./assets/golos-text-latin-500-normal-BznAvurO.woff2","./assets/golos-text-latin-700-normal-CxmN_Nfd.woff2","./assets/index-BXlvW71M.js","./assets/index-By2XxHsL.css","./assets/logbook-ChAJyO1t.js","./icons/icon-192.png","./icons/icon-512.png","./icons/mark.svg","./index.html","./lights/CREDITS.md","./lights/tell-tales.ttf","./manifest.webmanifest","./manual-mokka-my16-bg.pdf","./protokol-ptp.pdf"];
+const CACHE = "blitz-0f6feea082f6";
 /* PDF-ите не се променят между билдове. Държат се в отделен кеш с постоянно име,
    за да не се теглят 7 MB наново при всеки деплой. Шрифтовете НЕ са тук: малкият
    (tell-tales.ttf) се precache-ва при install, а едрите се кешират нормално. */
