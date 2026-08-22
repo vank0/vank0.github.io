2f0a5112d66f68e37560d6bae75ce2cebd2e4b33
Ivan Ivanov <ivan.ivanov1@inceptionai.ai>
2026-08-22 20:30:32 +0300

    Прозорецът се класифицира по това КОЙ е горил километрите: бензинов прогон вече не чупи газовия разход и дава истинско бензиново число

--- mokka/sw.js
+++ mokka/sw.js
@@ -2,8 +2,8 @@
    инжектират от Vite плъгина на build (public/ се копира дословно).
    Големите файлове (ръководството, 6.5MB) НЕ влизат в install списъка —
    те се кешират при първото отваряне, за да е бърза инсталацията. */
-const ASSETS = ["./assets/golos-text-cyrillic-400-normal-C7us6pn1.woff2","./assets/golos-text-cyrillic-500-normal-BSLQUuP1.woff2","./assets/golos-text-cyrillic-700-normal-BKmY45Ip.woff2","./assets/golos-text-latin-400-normal-Coi1FYaD.woff2","./assets/golos-text-latin-500-normal-BznAvurO.woff2","./assets/golos-text-latin-700-normal-CxmN_Nfd.woff2","./assets/index-By2XxHsL.css","./assets/index-DIlRk_JN.js","./assets/logbook-DT1rDBC6.js","./icons/icon-192.png","./icons/icon-512.png","./icons/mark.svg","./index.html","./lights/CREDITS.md","./lights/tell-tales.ttf","./manifest.webmanifest","./manual-mokka-my16-bg.pdf","./protokol-ptp.pdf"];
-const CACHE = "blitz-02f9dc731dbc";
+const ASSETS = ["./assets/golos-text-cyrillic-400-normal-C7us6pn1.woff2","./assets/golos-text-cyrillic-500-normal-BSLQUuP1.woff2","./assets/golos-text-cyrillic-700-normal-BKmY45Ip.woff2","./assets/golos-text-latin-400-normal-Coi1FYaD.woff2","./assets/golos-text-latin-500-normal-BznAvurO.woff2","./assets/golos-text-latin-700-normal-CxmN_Nfd.woff2","./assets/index-BHZ9XVv3.js","./assets/index-By2XxHsL.css","./assets/logbook-Bb3x_cWA.js","./icons/icon-192.png","./icons/icon-512.png","./icons/mark.svg","./index.html","./lights/CREDITS.md","./lights/tell-tales.ttf","./manifest.webmanifest","./manual-mokka-my16-bg.pdf","./protokol-ptp.pdf"];
+const CACHE = "blitz-6cfdcdc8eac2";
 /* PDF-ите не се променят между билдове. Държат се в отделен кеш с постоянно име,
    за да не се теглят 7 MB наново при всеки деплой. Шрифтовете НЕ са тук: малкият
    (tell-tales.ttf) се precache-ва при install, а едрите се кешират нормално. */
